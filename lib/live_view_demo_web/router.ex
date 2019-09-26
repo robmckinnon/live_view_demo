97d86bf9d3b1deb988eca8a6ec0d910efc8fb456
robmckinnon <rob@movingflow>
2019-09-26 15:34:58 +0100

    Add piano roll display with css grid-layout
    Add skeleton css.
    
    Remove phoenix css.

--- lib/live_view_demo_web/router.ex
+++ lib/live_view_demo_web/router.ex
@@ -17,10 +17,7 @@ defmodule LiveViewDemoWeb.Router do
   scope "/", LiveViewDemoWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
-    live "/clock", ClockLive
-
-    live "/midi", MidiLive
+    live "/", MidiLive
   end
 
   # Other scopes may use custom stacks.
