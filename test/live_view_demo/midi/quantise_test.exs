c3da4a418e8a0f060cfe0cb59c57f113304a9953
robmckinnon <rob@movingflow>
2019-09-30 14:05:14 +0100

    Add quantise start_end_beats function
    To get quantised start/end time for grid display.

---
+++ test/live_view_demo/midi/quantise_test.exs
@@ -0,0 +1,17 @@
+defmodule LiveViewDemo.Midi.QuantiseTest do
+  @moduledoc false
+  use ExUnit.Case
+  alias LiveViewDemo.Midi.Quantise
+  doctest Quantise
+
+  # test "start beat" do
+  #   # time = 3_439_523.9000000004
+  #   # rel_time = 0.0
+  #   # duration = 876.0500000002794
+  #   # beats = 1.7521000000005589
+  #   #
+  #   # start_beat = 0
+  #
+  #   assert Quantise.start_beat(0, 125) === {:ones, 0}
+  # end
+end
