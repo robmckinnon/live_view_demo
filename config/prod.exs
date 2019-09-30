558073e15c119f665c36ea0e97a3ea266ba65ac7
robmckinnon <rob@movingflow>
2019-09-30 20:51:35 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_view_demo, LiveViewDemoWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [
+    scheme: System.get_env("SCHEME") || "https",
+    host: System.get_env("HOST"),
+    port: System.get_env("HOSTPORT") || 443
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
